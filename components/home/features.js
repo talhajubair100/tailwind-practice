a1800d3d62bfcc937e13d73c3a865e321beb1e17
talhajubair100 <talhajubair100.bd@gmail.com>
2023-08-01 20:25:00 +0600

    flex practice

--- components/home/features.js
+++ components/home/features.js
@@ -2,6 +2,34 @@ const Features = () => {
     return (
         <>
             <div className="container">
+                <h1 className="text-center bg-primary text-xl font-medium text-white">This is flex practice section</h1>
+                <div className="h-80 bg-gray-100 flex flex-col items-center justify-between">
+                    <div className="p-2 bg-yellow-400">block 1</div>
+                    <div className="p-2 bg-blue-400 h-20">block 2</div>
+                    <div className="p-2 bg-red-400">block 3</div>
+                    <div className="p-2 bg-green-400">block 4</div>
+                    <div className="p-2 bg-pink-400">block 5</div>
+
+                </div>
+
+                <div className="mt-12 h-80 bg-gray-100 flex items-center content-start">
+                    <div className="p-2 bg-yellow-400">block 1</div>
+                    <div className="p-2 bg-blue-400 h-20">block 2</div>
+                    <div className="p-2 bg-red-400">block 3</div>
+                    <div className="p-2 bg-green-400">block 4</div>
+                    <div className="p-2 bg-pink-400">block 5</div>
+
+                </div>
+
+                <div className="mt-12 h-80 bg-gray-100 flex flex-col ">
+                    <div className="p-2 bg-yellow-400">block 1</div>
+                    <div className="p-2 bg-blue-400 ">block 2</div>
+                    <div className="p-2 bg-red-400">block 3</div>
+                    <div className="p-2 bg-green-400">block 4</div>
+
+                </div>
+
+                
 
             </div>
         </>
--- components/home/hero.js
+++ components/home/hero.js
@@ -2,7 +2,7 @@ const Hero = () => {
     return (
         <div className="container">
             <div className="">
-
+                This is Tailwind Practice page 
             </div>
         </div>
     )
